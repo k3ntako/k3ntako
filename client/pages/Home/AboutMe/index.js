8ae6259ab7e5467e7e181c018fffc4a448cebf15
k3ntako <kentarokaneki@gmail.com>
2019-09-12 11:04:49 -0400

    Nav bar fix

--- client/pages/Home/AboutMe/index.js
+++ client/pages/Home/AboutMe/index.js
@@ -9,6 +9,7 @@ export default class AboutMeSection extends Component {
     super(props);
     this.state = {
       isSticky: false,
+      windowHeight: 0,
       navOffsetTop: 0,
       me: {
         firstname: "Kentaro",
@@ -34,19 +35,38 @@ export default class AboutMeSection extends Component {
   componentDidMount(){
     this.navBar = document.getElementById(styles.nav);
 
-    const navOffsetTop = this.navBar.offsetTop;
-    this.setState({ navOffsetTop },
-      () => window.addEventListener('scroll', this.onScroll));
+    const windowHeight = window.innerHeight;
+    const navOffsetTop = windowHeight - this.navBar.offsetHeight;
+
+    this.setState({ navOffsetTop, windowHeight },
+      () => {
+        window.addEventListener('scroll', this.onScroll);
+        window.addEventListener('resize', this.onResize);
+      });
   }
 
   componentWillUnmount(){
     window.removeEventListener('scroll', this.onScroll);
+    window.removeEventListener('resize', this.onResize);
   }
 
   onScroll = () => {
     const isSticky = window.pageYOffset >= this.state.navOffsetTop;
     if( this.state.isSticky !== isSticky ){
       this.setState({ isSticky });
+    }
+  }
+
+  onResize = () => {
+    const windowHeight = window.innerHeight;
+    const navOffsetTop = windowHeight - this.navBar.offsetHeight;
+    const isSticky = window.pageYOffset >= this.state.navOffsetTop;
+    if( this.state.windowHeight !== windowHeight ){
+      this.setState({
+        windowHeight,
+        navOffsetTop,
+        isSticky,
+      });
     }
   }
 
@@ -69,6 +89,7 @@ export default class AboutMeSection extends Component {
         <a href="#aboutMe">About Me</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
+        <a href="#contactMe">Contact Me</a>
       </div>
     </div>
   }
